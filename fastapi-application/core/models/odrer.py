911409be421ce7bee07f535bd4470a475022247c
LESIUK <iuriilesyuk@gmail.com>
2024-08-28 11:30:46 +0300

    create relationships between user and post

--- fastapi-application/core/models/odrer.py
+++ fastapi-application/core/models/odrer.py
@@ -1,11 +1,14 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
-from sqlalchemy import Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Order(IntIdPkMixin, Base):
@@ -16,3 +19,5 @@ class Order(IntIdPkMixin, Base):
     date_create: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow())
     description: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
     # price: Mapped[int] = mapped_column(Integer)
+
+    user: Mapped["User"] = relationship(back_populates="orders")
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,10 +1,17 @@
-from sqlalchemy import UniqueConstraint, String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from .odrer import Order
 
 
 class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str] = mapped_column(String)
+
+    orders: Mapped[list["Order"]] = relationship(back_populates="user")
